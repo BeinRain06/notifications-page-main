b94608b4201958edd952019feb7d16a359a102f4
Beinrain06 <elngoueni@gmail.com>
2023-01-28 04:41:01 -0500

    First Commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,14 @@
+import Mark from "./avatar-mark-webber.webp";
+import Angela from "./avatar-angela-gray.webp";
+import Jacob from "./avatar-jacob-thompson.webp";
+import Rizky from "./avatar-rizky-hasanuddin.webp";
+import Kimberly from "./avatar-kimberly-smith.webp";
+import Nathan from "./avatar-nathan-peterson.webp";
+import Anna from "./avatar-anna-kim.webp";
+import chessImage from "./image-chess.webp";
+import DesktopScreenShot from "../screenshot/DeskTop_Screenshot.png";
+import MobileScreenShot from "../screenshot/Mobile_Screenshot.png";
+
+export { Mark, Angela, Jacob, Rizky, Kimberly, Nathan, Anna, chessImage };
+
+export { DesktopScreenShot, MobileScreenShot };
